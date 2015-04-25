17920943c360556e67145a2b4928b43bef1f4328
LoadLow <loadlow@users.noreply.github.com>
2015-04-25 18:15:06 +0200

    Update config file

--- src/config/config.py
+++ src/config/config.py
@@ -11,13 +11,17 @@ config = {  # details required to login to twitch IRC server
                     'run_cron': False,
                     # set this to false if you want don't want to run the cronjob but you want to preserve the messages etc
                     'run_time': 5,  # time in seconds
-                    'cron_messages': [
-                        'This is channel_one cron message one.',
-                        'This is channel_one cron message two.'
-                    ]
+                    'cron_messages': [],
+                    'refresh_moderators' : False,
+                    'refresh_moderators_interval': 10
+                },
+                '#loadlow': {
+                    'run_cron': False,
+                    'refresh_moderators' : True,
+                    'refresh_moderators_interval': 10
                 }
             },  # if set to true will display any data received
             'log_messages': True,
-            'socket_buffer_size': 2048  # maximum amount of bytes to receive from socket - 1024-4096 recommended
-            }
+            'socket_buffer_size': 2048,  # maximum amount of bytes to receive from socket - 1024-4096 recommended
+        }
 
